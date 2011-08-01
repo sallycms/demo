630ab7da4285cee4e26de4e74ecfa80418b425ff
Christoph <christoph@webvariants.de>
2011-08-02 01:31:26 +0200

    updated form module

--- develop/modules/form.input.php
+++ develop/modules/form.input.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * @sly name         form
- * @sly title        Formular
- * @sly description  zeigt ein auswählbares Formulartemplate an
+ * @sly name  form
+ * @sly title Formular
  */
 
 ?>
-<div class="wvModule">
-	<label>Template</label>
-	<select name="VALUE[5]" id="VALUE[5]" style="margin-bottom: 20px;">
-	<?
+<label>Template</label>
+<select name="VALUE[template]" id="VALUE[template]" style="margin-bottom: 20px;">
+<?
 
-	$service   = sly_Service_Factory::getTemplateService();
-	$templates = $service->getTemplates('form');
-	
-	foreach ($templates as $name => $title) {
-		$selected = '';
-		if ('REX_VALUE[5]' == $name) $selected = ' selected="selected"';
-		print '<option value="'.$name.'"'.$selected.'>'.sly_html($title).'</option>';
-	}
-	?>
-	</select>
-</div>
+$service   = sly_Service_Factory::getTemplateService();
+$templates = $service->getTemplates('form');
+
+foreach ($templates as $name => $title) {
+	$selected = '';
+	if ('REX_VALUE[template]' == $name) $selected = ' selected="selected"';
+	print '<option value="'.$name.'"'.$selected.'>'.sly_html($title).'</option>';
+}
+?>
+</select>
--- develop/modules/form.output.php
+++ develop/modules/form.output.php
@@ -1,30 +1,24 @@
 <?php
 /**
- * @sly name         form
- * @sly title        Formular
- * @sly description  zeigt ein auswählbares Formulartemplate an
+ * @sly name  form
+ * @sly title Formular
  */
 
+$template = 'SLY_VALUE[template]';
 
 if (sly_Core::isBackend()) {
-	$templateName = 'REX_VALUE[5]';
-	if (empty($templateName)) $templateName = 'Es wurde kein Template angegeben';
+	if (empty($template)) $template = 'Es wurde kein Template angegeben';
 	?>
-	<div class="wvModule">
-		<label>Template</label>
-		<?= sly_html($templateName) ?>
-	</div>
+	<label>Template:</label> <?= sly_html($template) ?>
 	<?
 }
 else {
 	?>
 	<form action="<?= sly_html($_SERVER['REQUEST_URI']) ?>" method="post" enctype="multipart/form-data">
 		<div class="hidden">
-			<input type="hidden" name="<?= WV30_Form::FORM_ID_FIELD_NAME ?>" value="REX_VALUE[5]" />
+			<input type="hidden" name="<?= WV30_Form::FORM_ID_FIELD_NAME ?>" value="<?= $template ?>" />
 		</div>
-		<? sly_Service_Factory::getTemplateService()->includeFile('REX_VALUE[5]') ?>
+		<? sly_Service_Factory::getTemplateService()->includeFile($template) ?>
 	</form>
 	<?
 }
-
-?>
