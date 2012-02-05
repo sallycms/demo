263ef968e2422ce3e160c807a185a9d771634ec7
Christoph <christoph@webvariants.de>
2012-02-05 04:23:05 +0100

    fixed wymeditor module

--- develop/modules/wymeditor.input.php
+++ develop/modules/wymeditor.input.php
@@ -4,11 +4,11 @@
  * @sly title Texteditor
  */
 
-if (!class_exists('sly_Form_WYMEditor')) {
+if (!class_exists('sly_Form_Wymeditor')) {
 	throw new sly_Exception('Das WYMeditor-AddOn muss aktiviert sein, um dieses Modul zu verwenden.');
 }
 
-$editor = new sly_Form_WYMEditor('html', 'Fließtext', $values->get('html'));
+$editor = new sly_Form_Wymeditor('html', 'Fließtext', $values->get('html'));
 $editor->useFullWidth();
 
 $form->add($editor);
