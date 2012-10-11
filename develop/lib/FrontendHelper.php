b224768f8a00a1404729c4b7346d81f50a25e09e
Christoph <christoph@webvariants.de>
2012-10-11 23:43:50 +0200

    adjusted to changes in image_resize

--- develop/lib/FrontendHelper.php
+++ develop/lib/FrontendHelper.php
@@ -86,7 +86,7 @@ class FrontendHelper {
 		$service = sly_Service_Factory::getAddOnService();
 
 		if ($service->isAvailable('image_resize')) {
-			$text = A2_Thumbnail::scaleMediaImagesInHtml($text, 200);
+			$text = A2_Util::scaleMediaImagesInHtml($text, 200);
 		}
 
 		if ($service->isAvailable('developer_utils')) {
